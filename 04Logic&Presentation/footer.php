6a6adcaf31159bfefe9b9812e0ff48bd2a6837db
TobiasAndren <tobiasandren@hotmail.com>
2024-09-23 13:34:01 +0200

    Add files via upload

---
+++ 04Logic&Presentation/footer.php
@@ -0,0 +1,8 @@
+<?php
+?>
+
+<body>
+    <footer>
+        <small>&copy; <?php echo date("Y") ?> Seinfeld</small>
+    </footer>
+</body>
---
+++ 04Logic&Presentation/index.php
@@ -0,0 +1,21 @@
+<?php
+require_once __DIR__ . "/header.php";
+?>
+
+<body>
+    <article>
+        <img
+            src="https://media.giphy.com/media/xT1XGXgtj8PWdvCLFS/giphy.gif"
+            alt="Elaine Benes dancing" />
+        <p>
+            Seinfeld is an American television sitcom that originally aired on NBC
+            from July 5, 1989, to May 14, 1998. It lasted nine seasons, and is now
+            in syndication.
+        </p>
+        <p><a href="about.php">Read more…</a></p>
+    </article>
+</body>
+
+<?php
+require_once __DIR__ . "/footer.php";
+?>
